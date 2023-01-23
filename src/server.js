d40db92905b7d7432474ed9805882752653c2179
Welton <rwelton284@gmail.com>
2023-01-23 09:46:04 -0300

    first commit

---
+++ src/server.js
@@ -0,0 +1,19 @@
+require("dotenv").config()
+
+const express = require('express')
+const routes = require("./routes")
+const connectToDatabase = require("./database")
+const cors = require("cors")
+
+connectToDatabase();
+
+const app = express();
+const PORT = 3690;
+
+app.use(cors())
+app.use(express.json())
+app.use(routes);
+
+app.listen(PORT, () =>{
+    console.log(`⚡ Server running on http://localhost:${PORT}`);
+})
